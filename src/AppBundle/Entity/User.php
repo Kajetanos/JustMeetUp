dc905bacc15a12853bdfa550ec61907cd9916f88
Kajetanos <kajetanos75@gmail.com>
2018-07-30 22:18:23 +0200

    configuration HwiBundle to login by googleplus and github- cant test on local server.

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -52,7 +52,6 @@
 //    protected $email; 
 //    
 //}
-
 // src/AppBundle/Entity/User.php
 
 namespace AppBundle\Entity;
@@ -64,18 +63,69 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser
-{
+class User extends BaseUser {
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-             
-    public function __construct()
-    {
+
+    /** @ORM\Column(name="googleplus_id", type="string", length=255, nullable=true) */
+    protected $googleplus_id;
+
+    /** @ORM\Column(name="googleplus_access_token", type="string", length=255, nullable=true) */
+    protected $googleplus_access_token;
+
+    /** @ORM\Column(name="github_id", type="string", length=255, nullable=true) */
+    protected $github_id;
+
+    /** @ORM\Column(name="github_access_token", type="string", length=255, nullable=true) */
+    protected $github_access_token;
+
+    public function __construct() {
         parent::__construct();
+    }
+
+    public function setGoogleplusId($googlePlusId) {
+        $this->googleplus_id = $googlePlusId;
+
+        return $this;
+    }
+
+    public function getGoogleplusId() {
+        return $this->googleplus_id;
+    }
+
+    public function setGoogleplusAccessToken($googleplusAccessToken) {
+        $this->googleplus_access_token = $googleplusAccessToken;
+
+        return $this;
+    }
+
+    public function getGoogleplusAccessToken() {
+        return $this->googleplus_access_token;
+    }
+
+    public function setGithubId($githubId) {
+        $this->github_id = $githubId;
+
+        return $this;
+    }
+
+    public function getGithubId() {
+        return $this->github_id;
+    }
+
+    public function setGithubAccessToken($githubAccessToken) {
+        $this->github_access_token = $githubAccessToken;
+
+        return $this;
+    }
+
+    public function getGithubAccessToken() {
+        return $this->github_access_token;
     }
 
 }
